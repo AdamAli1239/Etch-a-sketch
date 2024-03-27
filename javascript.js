6091e277f3e3336196d89ed9777db38200eb02f7
Adam Ali <154936108+AdamAli72@users.noreply.github.com>
2024-03-27 01:02:09 -0400

    made the first box turn yellow when mouse hovers over it, need to make it change back after mouse leaves

--- javascript.js
+++ javascript.js
@@ -1,8 +1,18 @@
 const container = document.querySelector("#container");
 for(let i=0;i<256;i++){
     const div = document.createElement('div')
+    div.classList.add("haha")
     container.appendChild(div)
 
 }
 
 
+const selectedDiv = document.querySelector(".haha");
+
+
+selectedDiv.addEventListener("mouseover",event=>{
+    event.target.style.backgroundColor="yellow"
+})
+
+
+console.log(selectedDiv)
