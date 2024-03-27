ac0d2c7b353522790c12c44de9a9c5bdfb27e031
Adam Ali <154936108+AdamAli72@users.noreply.github.com>
2024-03-27 17:20:20 -0400

    added a button on the top that propts the user to enter the amount of squares for a new grid

--- javascript.js
+++ javascript.js
@@ -11,7 +11,20 @@ const selectedDiv = document.querySelectorAll(".haha");
 
 
 selectedDiv.forEach(element =>{
-    element.addEventListener("mouseover",event=>{
+    element.addEventListener("mouseenter",event=>{
         event.target.style.backgroundColor="yellow"
     })
+})
+
+selectedDiv.forEach(element =>{
+    element.addEventListener("mouseleave",event=>{
+        event.target.style.backgroundColor="blue"
+    })
+})
+
+const promptUser = document.querySelector("#button")
+
+promptUser.addEventListener("click",() =>{
+    let userInput = prompt("Please enter the number of squares per side for the new grid (max is 100)")
+   
 })
