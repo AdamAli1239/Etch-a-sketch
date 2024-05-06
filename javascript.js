51560e292b91c743572a293b8bdd8c4707853d1f
Adam Ali <154936108+AdamAli72@users.noreply.github.com>
2024-05-06 19:01:51 -0400

    made it so that the user can change the number of squares on the page

--- javascript.js
+++ javascript.js
@@ -7,7 +7,20 @@ for(let i=0;i<256;i++){
 }
 
 
-const selectedDiv = document.querySelectorAll(".haha");
+
+const promptUser = document.querySelector("#button")
+
+promptUser.addEventListener("click",() =>{
+    let userInput = prompt("Please enter the number of squares per side for the new grid (max is 100)")
+    container.innerHTML=''
+    //this gets rid of all the other squares that were in the container by setting the inner part to an empty string
+    let userInt = parseInt(userInput)
+    for (let i=0;i<userInt;i++){
+        const divvy = document.createElement('div')
+        divvy.classList.add("haha")
+        container.appendChild(divvy)
+    }
+    const selectedDiv = document.querySelectorAll(".haha");
 
 
 selectedDiv.forEach(element =>{
@@ -22,9 +35,20 @@ selectedDiv.forEach(element =>{
     })
 })
 
-const promptUser = document.querySelector("#button")
 
-promptUser.addEventListener("click",() =>{
-    let userInput = prompt("Please enter the number of squares per side for the new grid (max is 100)")
    
+})
+const selectedDiv = document.querySelectorAll(".haha");
+
+
+selectedDiv.forEach(element =>{
+    element.addEventListener("mouseenter",event=>{
+        event.target.style.backgroundColor="yellow"
+    })
+})
+
+selectedDiv.forEach(element =>{
+    element.addEventListener("mouseleave",event=>{
+        event.target.style.backgroundColor="blue"
+    })
 })
